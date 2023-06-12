b54eab14447caf955e62632adae9141e36f1e18b
Sarah Schätzchen <saphyll@t-online.de>
2023-06-12 15:18:29 +0200

    fix gaussian

--- gaussian.py
+++ gaussian.py
@@ -10,7 +10,7 @@ class kernel_type(Enum):
     SOAGK = 3
 
 class AGK:
-    def __init__(self, scales, anisotropies, orientations_num) -> None:
+    def __init__(self, scales, anisotropies, orientations_num, invert=True) -> None:
         """
         Zero order (anisotropic) Gaussian Filter by Lopez-Molina et al. [1]_ for 2D images.
         Parameters
@@ -38,6 +38,7 @@ class AGK:
         self.anisotropies = anisotropies
         self.orientations = np.arange(0, (orientations_num)*np.pi/orientations_num, np.pi/orientations_num)
         self.kernels = self._create_kernels()
+        self.invert = invert
 
     def _create_kernels(self):
         kernels = []
@@ -64,7 +65,7 @@ class AGK:
                                                                          np.max(self.anisotropies),
                                                                          self.orientations.size)
 
-    def compute(self, input, invert=False, get_directions=False):
+    def compute(self, input, get_directions=False):
         """
         Computes filter results for the given image and optionally returns a 
         directionality map for the result.
@@ -86,25 +87,30 @@ class AGK:
         out_directions : 2D ndarray, optional
             Direction of lowest curvature for each pixel.
         """
-        lineMap = np.empty(input.shape)
+        output = np.empty(input.shape)
         angleMap = np.empty(input.shape)
-        for i, orientation in enumerate(self.orientations):
-            for kernel in self.kernels[i]:
+        for o, orientation in enumerate(self.orientations):
+            for kernel in self.kernels[o]:
                     #invert kernel for detection of bright lines
-                    if invert == True:
+                    if self.invert == True:
                         kernel = kernel * -1
 
-                    output = cv2.filter2D(input, -1, kernel)
-                    for i in range(lineMap.shape[0]):
-                        indices = np.where(output[i] > lineMap[i])
+                    kernel_output = cv2.filter2D(input, -1, kernel)
+                    
+                    for i in range(output.shape[0]):
+                        indices = np.where(kernel_output[i] > output[i])
                         for j in indices[0]:
-                            lineMap[i][j] = output[i][j]
+                            output[i][j] = kernel_output[i][j]
                             angleMap[i][j] = orientation 
+        #normalize output
+        output = output - np.min(output)
+        output = output / np.max(output) 
+
         if get_directions == True:
-            angleMap = np.rad2deg(angleMap)
-            return lineMap, angleMap
+            angleMap = np.rad2deg(angleMap) + .5 # +.5 to avoid wrong bin assignments in histograms
+            return output, angleMap
         else:
-            return lineMap
+            return output
     
     def _gk(self, sigma, x, y):
         """
@@ -136,14 +142,14 @@ class AGK:
         """
         Rotation matrix for a given angle theta.
         """
-        return [[np.cos(theta), np.sin(theta)],
-                [-np.sin(theta), np.cos(theta)]]
+        return [[np.cos(theta), -np.sin(theta)],
+                [np.sin(theta), np.cos(theta)]]
     
     def _normalize(self, kernel):
         return kernel / np.sum(np.abs(kernel))
 
 class FOAGK(AGK):
-    def __init__(self, scales, anisotropies, orientations_num) -> None:
+    def __init__(self, scales, anisotropies, orientations_num, invert=True) -> None:
         """
         First order (anisotropic) Gaussian Filter by Lopez-Molina et al. [1]_ for 2D images.
         Parameters
@@ -195,7 +201,7 @@ class FOAGK(AGK):
         return kernels
 
 class SOAGK(AGK):
-    def __init__(self, scales, anisotropies, orientations_num) -> None:
+    def __init__(self, scales, anisotropies, orientations_num, invert=True) -> None:
         """
         Second order (anisotropic) Gaussian Filter by Lopez-Molina et al. [1]_ for 2D images.
         Parameters
